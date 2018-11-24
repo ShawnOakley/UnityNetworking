a0c3d0105b27b3617c451cb8235b08c6ae3e8347
Shawn Oakley <shawnoakley@gmail.com>
2018-11-24 00:27:05 -0500

    Adds client check to limit commands to own prefab.  Adds networking in simplest case.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,22 +1,25 @@
-﻿//adapted from example script available at
-//https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 10.0F;
-    public float rotationSpeed = 100.0F;
+	Rigidbody rb;
+    float speed = 25.0F;
+    float rotationSpeed = 50.0F;
 
-
-	// Update is called once per frame
+    void Start(){
+        rb = this.GetComponent<Rigidbody>();
+    }
+	
+    // Update is called once per frame
 	void Update () {
 	
         float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
+        Quaternion turn = Quaternion.Euler(0f,rotation,0f);
+        rb.MovePosition(rb.position + this.transform.forward * translation);
+        rb.MoveRotation(rb.rotation * turn);
 	}
 }
