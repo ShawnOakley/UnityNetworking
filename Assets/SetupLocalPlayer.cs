64d3cfd320fd1f997246c96ae9db413f95126d43
Shawn Oakley <shawnoakley@gmail.com>
2018-12-01 20:11:38 -0500

    Adds name for clients and sets name above car model

--- Assets/SetupLocalPlayer.cs
+++ Assets/SetupLocalPlayer.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class SetupLocalPlayer : NetworkBehaviour {
+
+    public Text namePrefab;
+    public Text nameLabel;
+    public Transform namePos;
 
 	public override void OnStartServer()
 	{
@@ -25,13 +30,21 @@ public class SetupLocalPlayer : NetworkBehaviour {
 		if(isLocalPlayer)
 		{
 			GetComponent<PlayerController>().enabled = true;
+            CameraFollow360.player = this.gameObject.transform;
 		}
 		else
 		{
 			GetComponent<PlayerController>().enabled = false;
 		}
+        GameObject canvas = GameObject.FindWithTag("MainCanvas");
+        nameLabel = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as Text;
+        nameLabel.transform.SetParent(canvas.transform);
 		Debug.Log("Starting Player " + isLocalPlayer);
 	}
 
-
+    private void Update()
+    {
+        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+        nameLabel.transform.position = nameLabelPos;
+    }
 }
